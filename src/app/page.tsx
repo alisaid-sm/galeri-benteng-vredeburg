cc2423e906c03da25e4f20995d71c0937db7fab3
alisaid-sm <alisaid7170@gmail.com>
2024-11-19 09:56:32 +0700

    button ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
     <main>
       <div className="flex flex-col h-screen">
         <div className="flex justify-center pt-6">
-          <span className="text-3xl">Museum Benteng Vredeburg</span>
+          <span className="text-4xl">Museum Benteng Vredeburg</span>
         </div>
         <div className="flex justify-center items-center flex-grow">
-          <p className="drop-shadow-lg shadow-black text-center p-8">
+          <p className="drop-shadow-lg shadow-black text-center p-8 text-lg">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
@@ -20,6 +20,20 @@ export default function Home() {
             PageMaker including versions of Lorem Ipsum.
           </p>
         </div>
+      </div>
+      <div className="flex justify-center w-full fixed bottom-5">
+        <button
+          type="button"
+          className="text-white bg-[#D46C4F] hover:bg-[#D46C4F] font-medium rounded-lg text-sm me-2 w-28 h-8"
+        >
+          Tentang Kami
+        </button>
+        <button
+          type="button"
+          className="text-[#D46C4F] bg-white hover:bg-white font-medium rounded-lg text-sm me-2 w-28 h-8 border-2 border-[#D46C4F]"
+        >
+          Galeri
+        </button>
       </div>
     </main>
   );
