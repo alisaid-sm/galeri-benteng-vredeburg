73be649bea4f0c84e86ce4bfebf42a75f1523ee3
alisaid-sm <alisaid7170@gmail.com>
2024-12-04 10:07:55 +0700

    card bg img

--- src/app/galeri/page.tsx
+++ src/app/galeri/page.tsx
@@ -1,10 +1,13 @@
 export default function Galeri() {
+  const card =
+    "bg-white text-white flex justify-center items-center rounded-md h-52";
+
   return (
     <main className="p-20">
       <div className="grid grid-cols-3 gap-5">
-        <div className="bg-white text-black flex justify-center items-center rounded-md h-52">Diorama 1</div>
-        <div className="bg-white text-black flex justify-center items-center rounded-md h-52">Diorama 2</div>
-        <div className="bg-white text-black flex justify-center items-center rounded-md h-52">Diorama 3</div>
+        <div className={`${card} bg-[url('/img/mainbg.svg')] bg-center`}>Diorama 1</div>
+        <div className={`${card} bg-[url('/img/mainbg.svg')] bg-center`}>Diorama 2</div>
+        <div className={`${card} bg-[url('/img/mainbg.svg')] bg-center`}>Diorama 3</div>
       </div>
     </main>
   );
