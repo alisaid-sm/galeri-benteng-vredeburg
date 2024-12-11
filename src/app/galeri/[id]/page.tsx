215a10ca821b550327827b0cde3e2889059a4b72
alisaid-sm <alisaid7170@gmail.com>
2024-12-11 09:21:20 +0700

    add real foto assets and initial detail page

--- src/app/galeri/[id]/page.tsx
+++ src/app/galeri/[id]/page.tsx
@@ -4,5 +4,24 @@ export default async function Page({
   params: Promise<{ id: string }>;
 }) {
   const id = (await params).id;
-  return <div className="h-screen bg-[url('/img/mainbg.svg')] bg-center bg-cover">My Post: {id}</div>;
+  return (
+    <div className={`h-screen bg-[url('/img/${id}')] bg-center bg-cover`}>
+      <div className="overlay-bg">
+        <h1 className="text-center">My Post: {id}</h1>
+      </div>
+      <div className="overlay-bg">
+        <p className="text-center">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/app/galeri/page.tsx
+++ src/app/galeri/page.tsx
@@ -4,9 +4,9 @@ export default function Galeri() {
   return (
     <main className="p-20">
       <div className="grid grid-cols-4 gap-5">
-        <div className="card bg-[url('/img/mainbg.svg')] bg-center">
+        <div className="card bg-[url('/img/1.jpg')] bg-center">
           <span className="absolute top-3">Foto 1</span>
-          <Link href="/galeri/1">
+          <Link href="/galeri/1.jpg">
             <button
               type="button"
               className="btn-2-active absolute bottom-3 left-1/2 translate-x-[-50%]"
@@ -15,9 +15,9 @@ export default function Galeri() {
             </button>
           </Link>
         </div>
-        <div className="card bg-[url('/img/mainbg.svg')] bg-center">
+        <div className="card bg-[url('/img/2.jpg')] bg-center">
           <span className="absolute top-3">Foto 2</span>
-          <Link href="/galeri/2">
+          <Link href="/galeri/2.jpg">
             <button
               type="button"
               className="btn-2-active absolute bottom-3 left-1/2 translate-x-[-50%]"
@@ -26,9 +26,9 @@ export default function Galeri() {
             </button>
           </Link>
         </div>
-        <div className="card bg-[url('/img/mainbg.svg')] bg-center">
+        <div className="card bg-[url('/img/3.jpg')] bg-center">
           <span className="absolute top-3">Foto 3</span>
-          <Link href="/galeri/3">
+          <Link href="/galeri/3.jpg">
             <button
               type="button"
               className="btn-2-active absolute bottom-3 left-1/2 translate-x-[-50%]"
@@ -37,9 +37,9 @@ export default function Galeri() {
             </button>
           </Link>
         </div>
-        <div className="card bg-[url('/img/mainbg.svg')] bg-center">
+        <div className="card bg-[url('/img/4.jpg')] bg-center">
           <span className="absolute top-3">Foto 4</span>
-          <Link href="/galeri/4">
+          <Link href="/galeri/4.jpg">
             <button
               type="button"
               className="btn-2-active absolute bottom-3 left-1/2 translate-x-[-50%]"
