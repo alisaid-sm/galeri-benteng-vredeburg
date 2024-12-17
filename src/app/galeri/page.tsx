dbd45f5cdb874bfaf00a353046351cacddfebf26
alisaid-sm <alisaid7170@gmail.com>
2024-12-17 09:35:14 +0700

    implement vertical scroll on fotos

--- src/app/galeri/page.tsx
+++ src/app/galeri/page.tsx
@@ -5,7 +5,7 @@ export default function Galeri() {
   const arrayOfFotos : TArrayOfFoto[] = arrayOfFoto;
 
   return (
-    <main className="p-20">
+    <main className="p-20 h-screen overflow-y-scroll">
       <div className="grid grid-cols-4 gap-5">
         {arrayOfFotos.map((obj) => (
           <div key={obj.id} className={`card bg-center`} style={{background: `url('/img/${obj.image}')`}}>
